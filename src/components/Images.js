9558930c422c0b5870aa55dbdf5d55a528cac816
Anjani Sowmya Bollapragada <asbollapragada@gmail.com>
2021-05-19 08:36:24 +0530

    Import images at one place

---
+++ src/components/Images.js
@@ -0,0 +1,23 @@
+import hair_care_banner from "../Assets/personalCareImages/hair-care-20200704.jpg"
+import hair_care_1 from "../Assets/personalCareImages/anmol-gold-pure-coconut-oil-500-ml-0-20201017.jpg"
+import hair_care_2 from "../Assets/personalCareImages/dabur-amla-hair-oil-450-ml-0-20201222 (1).jpg"
+import hair_care_3 from "../Assets/personalCareImages/bajaj-almond-drops-non-sticky-hair-oil-300-ml-0-20200924 (1).jpg"
+import hair_care_4 from "../Assets/personalCareImages/tresemme-keratin-smooth-shampoo-340-ml-0-20210112 (1).jpg"
+import hair_care_5 from "../Assets/personalCareImages/bio-green-apple-shampoo-650-ml-0-20200814 (1).jpg"
+import hair_care_6 from "../Assets/personalCareImages/dhathri-dheedhi-anti-hair-fall-herbal-shampoo-400-ml-0-20201020 (1).jpg"
+import hair_care_7 from "../Assets/personalCareImages/dhathri-dheedhi-anti-dandruff-herbal-shampoo-400-ml-0-20201020 (2).jpg"
+import hair_care_8 from "../Assets/personalCareImages/parachute-100-pure-coconut-hair-oil-200-ml-0-20201020 (1).jpg"
+const images = {
+    hair_care_banner,
+    hair_care_1,
+    hair_care_2,
+    hair_care_3,
+    hair_care_4,
+    hair_care_5,
+    hair_care_6,
+    hair_care_7,
+    hair_care_8
+
+}
+
+export default images
