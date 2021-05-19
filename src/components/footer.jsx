6b271c4cfc9d44b50be837a8434947d2e75abc7e
Anjani Sowmya Bollapragada <asbollapragada@gmail.com>
2021-05-19 10:14:24 +0530

    fixed errors in links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,11 +11,11 @@ class Footer extends React.Component{
           {/* <!--Footer Content Mostpopular list/ hidded on small screens--> */}
             <ul className="col-md-3 pl-5 ml-1 mbl">
                 <li><h2>MOST POPULAR CATEGORIES</h2></li>
-                <li><Link to="/content/fruits_vegetables/fresh_fruits">Staples</Link></li>
+                <li><Link to="/content/fruits_vegetables/fresh_fruits">Fruits and Vegetables</Link></li>
                 <li><Link to="/content/beverages/tea">Bevereges</Link></li>
                 <li><Link to="/content/personal_care/hair_care">Personal Care</Link></li>
                 <li><Link to="/">Home Care</Link></li>
-                <li><Link to="/content/staples/dry_fruits">Fruits and Vegetables</Link></li>
+                <li><Link to="/content/staples/dry_fruits">Staples</Link></li>
                 <li><Link to="/">Baby Care</Link></li>
                 <li><Link to="/">Snacks & Branded Foods</Link></li>
                 <li><Link to="/">Dairy & Bakery</Link></li>
