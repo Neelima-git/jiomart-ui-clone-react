ecc232b0b7a80c6858660a84513d2e22884ad57d
Neelima-git <bellana.neelima2@gmail.com>
2021-05-19 13:03:48 +0530

    .

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/jiomart-ui-clone-react">
+            <Route path="/">
               <Homepage />
             </Route>
             <Route path="/content/:category/:sub_category">
